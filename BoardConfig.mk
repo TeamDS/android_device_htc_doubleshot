0cf946e3f125bc221932fd059bcfc563e19047ac
J Parker <kornyone@gmail.com>
2011-10-04 23:41:51 -0500

    Fixing keyboard light sensors (and kernel borkification)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,7 +78,7 @@ TARGET_HAVE_HDMI_OUT := true
 #TARGET_HARDWARE_3D := true
 
 # Define Prebuilt kernel locations
-TARGET_PREBUILT_KERNEL := device/htc/shooter/kernel
+TARGET_PREBUILT_KERNEL := device/htc/doubleshot/kernel
 
 # cat /proc/emmc
 # dev:        size     erasesize name
