84dfa36dbb3b05ba41fef1dc56decb6e83ca9efb
Emmanuel Utomi <emmanuelutomi@gmail.com>
2012-05-09 18:01:50 -0500

    Add stuff from cm.mk just in case not building a cyanogenmod rom

--- device_doubleshot.mk
+++ device_doubleshot.mk
@@ -14,6 +14,19 @@
 # limitations under the License.
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := doubleshot
+
+# Device naming
+PRODUCT_DEVICE := doubleshot
+PRODUCT_NAME := cm_doubleshot
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := myTouch_4G_Slide
+PRODUCT_MANUFACTURER := HTC
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_doubleshot BUILD_FINGERPRINT=tmous/htc_doubleshot/doubleshot:4.0.3/IML74K/275847.101:user/release-keys PRIVATE_BUILD_DESC="3.25.401.101 CL275847 release-keys" BUILD_NUMBER=27584
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
