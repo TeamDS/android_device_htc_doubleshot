e80e62f4519e4aa12837e30f597f27d7351a0f6e
Jason Parker <kornyone@gmail.com>
2012-06-06 17:55:23 -0500

    Updating TMOUS fingerprint build id to match pyramid.
    We still properly request the doubleshot device, this is simply numerical builds numbers from vendor

--- cm.mk
+++ cm.mk
@@ -20,4 +20,4 @@ PRODUCT_MODEL := myTouch_4G_Slide
 PRODUCT_MANUFACTURER := HTC
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_doubleshot BUILD_FINGERPRINT=tmous/htc_doubleshot/doubleshot:4.0.3/IML74K/275847.101:user/release-keys PRIVATE_BUILD_DESC="3.25.401.101 CL275847 release-keys" BUILD_NUMBER=275847
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_doubleshot BUILD_FINGERPRINT=tmous/htc_doubleshot/doubleshot:4.0.3/IML74K/356011.14:user/release-keys PRIVATE_BUILD_DESC="3.32.531.14 CL356011 release-keys" BUILD_NUMBER=356011
