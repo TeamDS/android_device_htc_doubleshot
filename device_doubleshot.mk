69379ff0d57f2a1b9fe513144ac78107741a7220
Jason Parker <kornyone@gmail.com>
2012-12-26 05:32:33 -0600

    Using prebuilt sensors blob from CM9 to prevent bootloop

--- device_doubleshot.mk
+++ device_doubleshot.mk
@@ -26,10 +26,6 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/doubleshot/overlay
 PRODUCT_PACKAGES += \
     gps.doubleshot
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.doubleshot
-
 # Wifi
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4329/device-bcm.mk)
 
